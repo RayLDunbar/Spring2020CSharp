3820909729b5501dc22d46dd1109b28394c7072a
RayLDunbar <rayldunbar@gmail.com>
2020-01-28 18:51:03 -0500

    add loop to input multiple students

--- src/Program.cs
+++ src/Program.cs
@@ -8,40 +8,11 @@ namespace RayLDunbar.CodeLou.ExerciseProject
             string continueRunning = "Y";
             while (continueRunning == "Y")
             {
-                InputStudent();
+                Student.AddStudent();
                 Console.WriteLine("Do you want to enter another student?");
-                continueRunning.ToUpper() = Console.ReadLine();
+                continueRunning = Console.ReadLine().ToUpper();
             }
-            Console.ReadKey();
         }
 
-        static void InputStudent()
-        {
-            Console.WriteLine("Enter Student Id");
-            var studentId = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter First Name");
-            var studentFirstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            var studentLastName = Console.ReadLine();
-            Console.WriteLine("Enter Class Name");
-            var className = Console.ReadLine();
-            Console.WriteLine("Enter Last Class Completed");
-            var lastClass = Console.ReadLine();
-            Console.WriteLine("Enter Last Class Completed Date in format MM/dd/YYYY");
-            var lastCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Start Date in format MM/dd/YYYY");
-            var startDate = DateTimeOffset.Parse(Console.ReadLine());
-            var studentRecord = new Student();
-            studentRecord.StudentId = studentId;
-            studentRecord.FirstName = studentFirstName;
-            studentRecord.LastName = studentLastName;
-            studentRecord.ClassName = className;
-            studentRecord.StartDate = startDate;
-            studentRecord.LastClassCompleted = lastClass;
-            studentRecord.LastClassCompletedOn = lastCompletedOn;
-            Console.WriteLine($"Student Id | Name |  Class "); ;
-            Console.WriteLine($"{studentRecord.StudentId} | {studentRecord.FirstName} {studentRecord.LastName} | {studentRecord.ClassName} ");
-            Console.ReadKey();
-        }
     }
 }
